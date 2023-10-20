e64f3c21964c9e48e4ba159757ad222b57b3ef5d
DESKTOP-PN6OK2K\sung <ronaldo39@naver.com>
2023-10-20 17:52:09 +0900

    기능 추가

--- javaWorkspace/subwayPrj/src/com/kh/subway/store/controller/SubwayController.java
+++ javaWorkspace/subwayPrj/src/com/kh/subway/store/controller/SubwayController.java
@@ -1,5 +1,6 @@
 package com.kh.subway.store.controller;
 
+import java.util.HashMap;
 import java.util.List;
 
 import com.kh.subway.main.Main;
@@ -18,15 +19,15 @@ import com.kh.subway.store.vo.SubwayVo;
 	
 	//관리자 메뉴
 	public void adminMenu() {
-		System.out.println("1. 매장 정보 수정 (관리자만");
-		System.out.println("2. 폐업 매장 삭제 (관리자만");
-		System.out.println("3. 신규 매장 추가 (관리자만");
+		System.out.println("1. 매장 이름 수정 (관리자만)");
+		System.out.println("2. 폐업 매장 삭제 (관리자만)");
+		System.out.println("3. 신규 매장 추가 (관리자만)");
 		
 		String num = Main.SC.nextLine();
 		switch(num) {
-		case "1" : break;
-		case "2" : break;
-		case "3" : break;
+		case "1" : changeStoreName(); break;
+		case "2" : closeStore(); break;
+		case "3" : newStore(); break;
 	    default : System.out.println("잘못 입력하셨습니다.");
 		}
 	}
@@ -35,15 +36,13 @@ import com.kh.subway.store.vo.SubwayVo;
 		System.out.println("=====SUBWAY STORE=====");
 		System.out.println("1. 역 주위 서브웨이 매장 전체 조회");
 		System.out.println("2. 역 주위 서브웨이 매장 조회 (역 이름)");
-		System.out.println("3. 매장 정보 수정 (관리자만)");
-		System.out.println("4. 폐업 매장 삭제 (관리자만)");
-		System.out.println("5. 신규 매장 추가 (관리자만)");
+		
 		
 		String num = Main.SC.nextLine();
 		switch(num) {
 		case "1" : subwayList(); break;
 		case "2" : subwayListByName(); break;
-		case "3" : break;
+		case "9" : return;
 		default : System.out.println("잘못 입력하셨습니다."); 
 		
 		}
@@ -89,54 +88,49 @@ import com.kh.subway.store.vo.SubwayVo;
 	public void subwayListByName() {
 		
 		try {
-			System.out.println("역 주위 매장 조회 (역 이름)");
+			System.out.println("===== 역 주위 매장 조회 (역 이름) =====");
 			
 			//데이터
 			System.out.println("조회할 매장 주위 역 이름 : ");
 			String name = Main.SC.nextLine();
 			
 			//서비스 호출
-			SubwayVo vo = service.subwayListByName(name);
+			List<SubwayVo> voList = service.subwayListByName(name);
 			
 			//결과 처리
-			if(vo == null) {
-				throw new Exception("역 주위 매장 상세조회 실패 ...");
+			if(voList.size() == 0) {
+				throw new Exception("잘못 입력하셨습니다.");
+			}	
+			for(SubwayVo vo : voList) {
+				System.out.println("일련번호 : " + vo.getStoreNo());
+				System.out.println("전화번호 : " + vo.getTel());
+				System.out.println("매장명 : " + vo.getStoreName());
+				System.out.println("주소 : " + vo.getAddress());
+				System.out.println();
 			}
-			System.out.println("====================================");
-			System.out.println("매장일련번호 " + vo.getStoreNo());
-			System.out.println("매장전화번호 " + vo.getTel());
-			System.out.println("매장명 " + vo.getStoreName());
-			System.out.println("매장 주소 " + vo.getAddress());
-			System.out.println("====================================");
 		}catch(Exception e) {
 			System.out.println("역 주위 매장 상세조회 실패 ...");
 			e.printStackTrace();
 		}
 	}
-
-	//관리자 계정으로 매장정보 수정
-	public void editStore() {
 	
-		try {
-			//관리자 계정인지 체크
-			if(Main.loginAdmin == null) {
-				throw new Exception("관리자 로그인이 필요한 서비스입니다.");
-			}
+
+	//관리자 계정으로 매장 이름 수정
+	public void changeStoreName() {
+		
+		try {			
 			//데이터
-			System.out.println("매장 번호 : ");
+			System.out.println("정보 변동사항이 있는 매장 일련번호를 입력하세요 : ");
 			String storeNo = Main.SC.nextLine();
-			System.out.println("변경사항이 있는 매장명 : ");
+			System.out.println("변경할 매장명 이름을 입력하세요 : ");
 			String storeName = Main.SC.nextLine();
-			System.out.println("변경할 주소지 : ");
-			String address = Main.SC.nextLine();
-			
-			SubwayVo vo = new SubwayVo();
+				
+			SubwayVo vo = new SubwayVo();	
 			vo.setStoreNo(storeNo);
 			vo.setStoreName(storeName);
-			vo.setAddress(address);
-			
+				
 			//서비스 호출
-			int result = service.editStore(vo);
+			int result = service.changeStoreName(vo);
 			
 			//결과처리
 			if(result == 1) {
@@ -146,6 +140,81 @@ import com.kh.subway.store.vo.SubwayVo;
 			}
 		}catch(Exception e) {
 			System.out.println("매장 주소지 변경 실패");
+			e.printStackTrace();
+		}		
+	}
+	/**
+	 * 폐업 매장 삭제(관리자만)
+	 * 
+	 * UPDATE SUBWAY
+	 * 	SET 
+	 * 		DEL_YN = 'Y'
+	 * 		,CLOSE_DATE = SYSDATE
+	 * 		,MODIFY_DATE = SYSDATE
+	 * WHERE STORE_NO = ?
+	 */
+	
+	//폐업 매장 등록(삭제)
+	public void closeStore() {
+		
+		try {
+			System.out.println("=====폐업한 매장 정보 삭제=====");
+			
+			//관리자 여부 체크
+			
+			//데이터
+			System.out.println("폐업한 매장 일련 번호를 입력하세요. : ");
+			String num = Main.SC.nextLine();
+			
+			//서비스 호출
+			HashMap<String, String> map = new HashMap<String, String>();
+			map.put("storeNo", num);
+			int result = service.closeStore(map);
+			
+			//결과 처리
+			if(result != 1) {
+				throw new Exception();
+			}
+			System.out.println("폐업 매장 등록(삭제) 완료");
+		}catch(Exception e) {
+			System.out.println("폐업 매장 등록(삭제) 실패");
+			e.printStackTrace();
+		}
+		
+	}
+	
+	//신규 매장 추가
+	public void newStore() {
+		
+		try {
+			System.out.println("===== 신규 매장 추가 =====");
+
+			//데이터
+			System.out.println("신규 매장 일련 번호를 입력하세요 : ");
+			String storeNo = Main.SC.nextLine();
+			System.out.println("신규 매장 이름을 입력하세요 : ");
+			String storeName = Main.SC.nextLine();
+			System.out.println("신규 매장 전화번호를 입력하세요 : ");
+			String tel = Main.SC.nextLine();
+			System.out.println("신규 매장 주소를 입력하세요 : ");
+			String address = Main.SC.nextLine();
+			
+			SubwayVo vo = new SubwayVo();
+			vo.setStoreNo(storeNo);
+			vo.setStoreName(storeName);
+			vo.setTel(tel);
+			vo.setAddress(address);
+			
+			//서비스 호출
+			int result = service.newStore(vo);
+			
+			//결과 처리
+			if(result != 1) {
+				throw new Exception();
+			}
+			System.out.println("신규 매장 추가 성공");
+		}catch(Exception e) {
+			System.out.println("매장 추가 실패 ...");
 			e.printStackTrace();
 		}
 	}
--- javaWorkspace/subwayPrj/src/com/kh/subway/store/dao/SubwayDao.java
+++ javaWorkspace/subwayPrj/src/com/kh/subway/store/dao/SubwayDao.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import com.kh.subway.store.vo.SubwayVo;
 
@@ -45,44 +46,45 @@ public class SubwayDao {
 	}
 
 	//입력한 역 주위 매장 조회
-	public SubwayVo subwayListByName(Connection conn, String name) throws Exception {
-	    String sql = "SELECT STORE_NO, STORE_NAME, TEL, ADDRESS FROM SUBWAY WHERE STORE_NAME LIKE ?";
+	public List<SubwayVo> subwayListByName(Connection conn, String name) throws Exception {
+	    String sql = "SELECT S.STORE_NO, S.STORE_NAME, S.TEL, S.ADDRESS FROM SUBWAY S JOIN STATION T ON S.STATION_NO = T.STATION_NO WHERE T.STATION_NAME LIKE '%' || ? || '%'";
 	    PreparedStatement pstmt = conn.prepareStatement(sql);
-	    pstmt.setString(1, "%" + name + "%"); // '%'를 추가하여 부분 일치 검색을 수행
-
+	    pstmt.setString(1, name); 
 	    ResultSet rs = pstmt.executeQuery();
 
-	    SubwayVo vo = null;
+	    List<SubwayVo> voList = new ArrayList<SubwayVo>();
 
-	    if (rs.next()) {
+	    while(rs.next()) {
+	    	
 	        String storeNo = rs.getString("STORE_NO");
 	        String storeName = rs.getString("STORE_NAME");
 	        String tel = rs.getString("TEL");
 	        String address = rs.getString("ADDRESS");
 
-	        vo = new SubwayVo();
+	        SubwayVo vo = new SubwayVo();
 	        vo.setStoreNo(storeNo);
 	        vo.setStoreName(storeName);
 	        vo.setTel(tel);
 	        vo.setAddress(address);
+	        
+	        voList.add(vo);
 	    }
 
 	    // close resources
 	    JDBCTemplate.close(rs);
 	    JDBCTemplate.close(pstmt);
 
-	    return vo;
+	    return voList;
 	}
 
-	//관리자 계정으로 매장 정보 수정
-	public int editStore(Connection conn, SubwayVo vo) throws Exception{
+	//관리자 계정으로 매장 이름 수정
+	public int changeStoreName(Connection conn, SubwayVo vo) throws Exception{
 	
 		//sql
-		String sql = "UPDATE SUBWAY SET STORE_NAME = ? , ADDRESS = ? WHERE STORE_NO = ?";
+		String sql = "UPDATE SUBWAY SET STORE_NAME = ? WHERE STORE_NO = ?";
 		PreparedStatement pstmt = conn.prepareStatement(sql);
 		pstmt.setString(1, vo.getStoreName());
-		pstmt.setString(2, vo.getAddress());
-		pstmt.setString(3, vo.getStoreNo());
+		pstmt.setString(2, vo.getStoreNo());
 		int result = pstmt.executeUpdate();
 		
 		//rs
@@ -91,5 +93,43 @@ public class SubwayDao {
 		JDBCTemplate.close(pstmt);
 		return result;
 	}
+	
+	//폐업 매장 등록(삭제)
+	public int closeStore(Connection conn, HashMap<String, String> map) throws Exception {
+
+		//sql
+		String sql = "UPDATE SUBWAY SET DEL_YN = 'Y' ,CLOSE_DATE = SYSDATE ,MODIFY_DATE = SYSDATE WHERE STORE_NO = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, map.get("storeNo"));
+		int result = pstmt.executeUpdate();
+		
+		//rs
+		
+		//close
+		JDBCTemplate.close(pstmt);
+		
+		return result;
+	}
+	
+	//신규 매장 추가
+	public int newStore(Connection conn, SubwayVo vo) throws Exception {
+		
+		//sql
+		String sql = "INSERT INTO SUBWAY(STORE_NO, STORE_NAME, TEL, ADDRESS) VALUES (?, ?, ?, ?) ORDER BY STORE_NO ASC";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, vo.getStoreNo());
+		pstmt.setString(2, vo.getStoreName());
+		pstmt.setString(3, vo.getTel());
+		pstmt.setString(4, vo.getAddress());
+		int result = pstmt.executeUpdate();
+		//rs
+		
+		//close
+		JDBCTemplate.close(pstmt);
+		
+		return result;
+		
+	}
+	
 
 }
